386be7194165090c462bb0d1772260ede2e9c507
plumzero <ytmrlimei@163.com>
2023-07-23 18:28:40 +0800

    stl

--- ch05/05_容器算法/05_02_next_permutation.cpp
+++ ch05/05_容器算法/05_02_next_permutation.cpp
@@ -6,27 +6,40 @@
 
 int main()
 {
-    {
-        std::vector<int> range { 1, 2, 3, 4 };
-        
-        do {
-            std::copy(std::begin(range), std::end(range), std::ostream_iterator<int>(std::cout, " "));
-            std::cout << std::endl;
-        } while (std::next_permutation(std::begin(range), std::end(range)));
-        
-        std::cout << std::endl;
-    }
+  {
+    std::vector<int> range { 1, 2, 3, 4 };
     
-    {
-        std::vector<int> range { 4, 2, 3, 1 };
-        
-        do {
-            std::copy(std::begin(range), std::end(range), std::ostream_iterator<int>(std::cout, " "));
-            std::cout << std::endl;
-        } while (std::next_permutation(std::begin(range), std::end(range)));
-        
-        std::cout << std::endl;
-    }
+    do {
+      std::copy(std::begin(range), std::end(range), std::ostream_iterator<int>(std::cout, " "));
+      std::cout << std::endl;
+    } while (std::next_permutation(std::begin(range), std::end(range)));
+    
+    std::cout << std::endl;
+  }
+  
+  {
+    std::vector<int> range { 4, 2, 3, 1 };
+    
+    do {
+      std::copy(std::begin(range), std::end(range), std::ostream_iterator<int>(std::cout, " "));
+      std::cout << std::endl;
+    } while (std::next_permutation(std::begin(range), std::end(range)));
+    
+    std::cout << std::endl;
+  }
 
-    return 0;
+  {
+    std::vector<int> range { 4, 2, 3, 1 };
+    
+    std::sort(range.begin(), range.end());
+
+    do {
+      std::copy(std::begin(range), std::end(range), std::ostream_iterator<int>(std::cout, " "));
+      std::cout << std::endl;
+    } while (std::next_permutation(std::begin(range), std::end(range)));
+    
+    std::cout << std::endl;
+  }
+
+  return 0;
 }
